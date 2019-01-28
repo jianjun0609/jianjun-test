a619924362380d456f09db63fd631b0bde84e547
niujianjun <niujianjun@lai-ai.com>
2019-01-28 11:21:04 +0800

    初始化项目

---
+++ src/main/java/com/jianjun/jianjuntest/JianjunTestApplication.java
@@ -0,0 +1,14 @@
+package com.jianjun.jianjuntest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JianjunTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JianjunTestApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/jianjun/jianjuntest/JianjunTestApplicationTests.java
@@ -0,0 +1,17 @@
+package com.jianjun.jianjuntest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JianjunTestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
